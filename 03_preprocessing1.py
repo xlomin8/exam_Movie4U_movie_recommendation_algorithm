3aa8405bb9b1b8936a7770a565985a058136f99f
oly-min <liberosm0818@naver.com>
2022-07-05 15:25:51 +0900

    preprocessing 1 - tokenize

--- 03_preprocessing1.py
+++ 03_preprocessing1.py
@@ -3,7 +3,7 @@ from konlpy.tag import Okt
 import re
 
 # 데이터 로드
-df = pd.read_csv('./crawling_data/reviews_2020_5.csv')
+df = pd.read_csv('./crawling_data/reviews_2019.csv')
 df.info()
 
 # 데이터 전처리
@@ -15,9 +15,23 @@ df_stopwords = pd.read_csv('./crawling_data/stopwords.csv')
 stopwords = list(df_stopwords['stopword'])
 
 ## 한글 제외 모두 제거
+count = 0
 clean_sentences = []
 for review in df.reviews:
+    count += 1
+    if count % 10 == 0:
+        print('.', end='')
+    if count % 100 == 0:
+        print()
     review = re.sub('[^가-힣 ]', ' ', review)  # review 문장 속 한글, 띄어쓰기 제외 모두 띄어쓰기로 대체
+    # review = review.split()
+    # words = []
+    # for word in review:
+    #     if len(word) > 20:
+    #         word = ' '
+    #         words.append(word)
+    # review = ' '.join(words)
+
     ## 형태소 분리
     token = okt.pos(review, stem=True)  # pos -> (형태소, 품사)형태로 분리
     df_token = pd.DataFrame(token, columns=['word', 'class'])
@@ -27,7 +41,7 @@ for review in df.reviews:
     ## 불용어 제거
     words = []
     for word in df_token.word:
-        ## 한 글자 제거
+        ## 한 글자, 20글자 이상 제거
         if len(word) > 1:
             if word not in stopwords:
                 words.append(word)
@@ -43,6 +57,6 @@ df = df[['title', 'clean_sentences']]
 df.dropna(inplace=True)
 
 # 저장
-df.to_csv('./crawling_data/clean_review_2020.csv', index=False)
+df.to_csv('./crawling_data/clean_review_2019.csv', index=False)
 
 
