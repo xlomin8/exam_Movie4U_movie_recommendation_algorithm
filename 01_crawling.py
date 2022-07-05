3a7f9aef346ae29d7981e503fd824ca6a774839b
oly-min <liberosm0818@naver.com>
2022-07-05 15:25:16 +0900

    movie reviews crawling

--- 01_crawling.py
+++ 01_crawling.py
@@ -36,15 +36,14 @@ review_xpath = '//*[@id="content"]/div[1]/div[4]/div[1]/div[4]'
 
 your_year = 2020    # 할당받은 연도로 수정
 # for i in range(1,38):
-for i in range(1,3):
+for i in range(13,38):
     url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page={}'.format(i)
     titles = []
     reviews = []
     try:
         # driver.get(url)
         ## 제목
-        # for j in range(1,21):
-        for j in range(1,3):
+        for j in range(1,21):
             driver.get(url)
             time.sleep(0.5)
             movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(j)
@@ -59,12 +58,15 @@ for i in range(1,3):
                 review_range = review_range.replace(',', '')    # 리뷰가 총 1,000이상일 때 숫자에서 ,제거
                 review_range = (int(review_range)-1) // 10 + 2  # 리뷰가 1페이지에 10개씩 존재
                 # 리뷰 모든 페이지
-                # for k in range(1, review_range):
-                for k in range(1, 3):
+                for k in range(1, review_range):
                     # 리뷰 ?페이지 버튼 누르기
                     review_page_button_xpath = '//*[@id="pagerTagAnchor{}"]'.format(k)
                     try:
-                        driver.find_element("xpath", review_page_button_xpath).click()  # 리뷰 페이지 버튼 클릭
+                        # 리뷰 버튼 xpath가 다른 경우
+                        try:
+                            driver.find_element("xpath", review_page_button_xpath).click()  # 리뷰 페이지 버튼 클릭
+                        except:
+                            driver.find_element("xpath", '//*[@id="movieEndTabMenu"]/li[5]/a').click()  # 리뷰 페이지 버튼 클릭
                         # 리뷰 한 페이지마다
                         for l in range(1, 11):
                             back_flag = False
@@ -90,7 +92,7 @@ for i in range(1,3):
             except:
                 print('movie : ', i, j)
         df = pd.DataFrame({'title':titles, 'reviews':reviews})
-        df.to_csv('./crawling_data/reviews_{}_{}page.csv'.format(your_year, i), index=False)
+        df.to_csv('./crawling_data/MovieReview_2020/reviews_{}_{}page.csv'.format(your_year, i), index=False)
     except:
         print('page : ', i) #몇번째 페이지에서 에러났는지
 driver.close()
