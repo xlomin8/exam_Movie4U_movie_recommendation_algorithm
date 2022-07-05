7d73163ca995aeb91e3288843b6e9f4c1d1a109b
oly-min <liberosm0818@naver.com>
2022-07-05 12:25:30 +0900

    data concat

--- 02_concat.py
+++ 02_concat.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import glob
+
 
 
 df = pd.DataFrame() # 빈 dataframe
+data_paths = glob.glob('./crawling_data/MovieReview_2020/*')
+print(data_paths)
 
-for i in range(1,3):
-    df_temp = pd.read_csv('./crawling_data/reviews_2020_{}page.csv'.format(i))
+for path in data_paths:
+    df_temp = pd.read_csv(path)
     # 결측치 제거
     df_temp.dropna(inplace=True)
     # 중복값 제거
@@ -15,4 +19,4 @@ for i in range(1,3):
 df.drop_duplicates(inplace=True)
 df.info()
 my_year = 2020
-df.to_csv('./crawling_data/reviews_{}.csv'.format(my_year))
+df.to_csv('./crawling_data/reviews_{}_5.csv'.format(my_year), index=False)
