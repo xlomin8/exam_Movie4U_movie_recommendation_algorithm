55dd3060e2d61f48bf4163101953bb6420e12e27
oly-min <liberosm0818@naver.com>
2022-07-06 10:20:35 +0900

    Term Frequency Inverse Document Frequency

--- 07_TFIDF.py
+++ 07_TFIDF.py
@@ -1,4 +1,4 @@
-# Term Frequency-Inverse Document Frequency : 문장 내 빈도수가 높을수록 가중치 큼 / 문서 내 빈도수가 높을수록 가중치 작음
+# TFIDF(Term Frequency-Inverse Document Frequency) : 문장 내 빈도수가 높을수록 가중치 큼 / 문서 내 빈도수가 높을수록 가중치 작음
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,7 +13,7 @@ df_reviews.info()
 Tfidf = TfidfVectorizer(sublinear_tf=True)
 Tfidf_matrix = Tfidf.fit_transform(df_reviews['reviews'])
 print(Tfidf_matrix.shape)
-# (3182, 84461) 3182개 영화,리뷰 / 84461개 단어
+# (3182, 84461) 3182개 영화,리뷰 * 84461개 단어
 
 print(Tfidf_matrix[0])
 # print(len(Tfidf_matrix[0]))
