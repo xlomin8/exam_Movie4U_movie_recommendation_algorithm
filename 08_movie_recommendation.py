f7967623d2ed0ae53a72060f007d3b9d442df15e
oly-min <liberosm0818@naver.com>
2022-07-06 10:54:16 +0900

    movie recommendation algorithm

--- 08_movie_recommendation.py
+++ 08_movie_recommendation.py
@@ -15,30 +15,31 @@ with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
 
-# # 영화 제목 / index 이용
-# movie_idx = df_reviews[df_reviews['titles']=='겨울왕국 2 (Frozen 2)'].index[0]
-# # print(movie_idx)
-# # print(df_reviews.iloc[1228, 1])
-#
-#
-# # 코사인 유사도
-# ## linear_kernel : 벡터 좌표값을 이용해 3182개의 코사인값 구해줌
-# cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
-#
-#
-# 코사인 유사도값(1에 가까울수록)으로 추천해주는 함수 생성
-# def getRecommendation(cosine_sim):
-#     simScore = list(enumerate(cosine_sim[-1]))
-#     ## 코사인 유사도값 기준으로 내림차순 정렬
-#     simScore = sorted(simScore, key=lambda  x:x[1], reverse=True)
-#     simScore = simScore[1:11]   # 0번은 자기자신(코사인 유사도 max)
-#     movieIdx = [i[0] for i in simScore]
-#     recMovieList = df_reviews.iloc[movieIdx, 0]
-#     return recMovieList
-#
-# # 추천 알고리즘
-# recommendation = getRecommendation(cosine_sim)
-# print(recommendation[1:11])
+# 영화 제목 / index 이용
+movie_idx = df_reviews[df_reviews['titles']=='겨울왕국 2 (Frozen 2)'].index[0]
+# print(movie_idx)
+# print(df_reviews.iloc[1228, 1])
+
+
+# 코사인 유사도
+## linear_kernel : 벡터 좌표값을 이용해 3182개의 코사인값 구해줌
+cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
+
+
+코사인 유사도값(1에 가까울수록)으로 추천해주는 함수 생성
+def getRecommendation(cosine_sim):
+    simScore = list(enumerate(cosine_sim[-1]))
+    ## 코사인 유사도값 기준으로 내림차순 정렬
+    simScore = sorted(simScore, key=lambda  x:x[1], reverse=True)
+    simScore = simScore[1:11]   # 0번은 자기자신(코사인 유사도 max)
+    movieIdx = [i[0] for i in simScore]
+    recMovieList = df_reviews.iloc[movieIdx, 0]
+    return recMovieList
+
+# 추천 알고리즘
+recommendation = getRecommendation(cosine_sim)
+print(recommendation[1:11])
+
 
 
 
@@ -101,5 +102,33 @@ print(recommendation[:10])
 
 
 
+
+# 3. 문장 이용한 추천 알고리즘
+# 문장 이용
+sentence = '견딜 수 없이 촌스런 삼남매의 견딜 수 없이 사랑스러운 행복소생기'
+
+
+# 모델 적용
+sentence_vec = Tfidf.transform([sentence])
+
+
+# 코사인 유사도
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+
+
+# 코사인 유사도값(1에 가까울수록)으로 추천해주는 함수 생성
+def getRecommendation(cosine_sim):
+    simScore = list(enumerate(cosine_sim[-1]))
+    ## 코사인 유사도값 기준으로 내림차순 정렬
+    simScore = sorted(simScore, key=lambda  x:x[1], reverse=True)
+    simScore = simScore[:11]   # 0번(코사인 유사도 max, 키워드 자기자신)은 처음부터 제외됐었으니 다시 포함
+    movieIdx = [i[0] for i in simScore]
+    recMovieList = df_reviews.iloc[movieIdx, 0]
+    return recMovieList
+
+
+# 추천 알고리즘
+recommendation = getRecommendation(cosine_sim)
+print(recommendation[:10])
 
 
